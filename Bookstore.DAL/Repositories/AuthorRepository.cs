af5099613e3591a41348bec9d066aff439f35b1a
kalev <vinderman771@gmail.com>
2025-03-02 13:18:04 +0300

    Докинул файл

---
+++ Bookstore.DAL/Repositories/AuthorRepository.cs
@@ -0,0 +1,7 @@
+﻿using Bookstore.DAL.Entities;
+using Bookstore.DAL.Interfaces;
+using Bookstore.EF;
+
+namespace Bookstore.DAL.Repositories;
+
+public class AuthorRepository(AppDbContext dbContext) : Repository<Author>(dbContext), IAuthorRepository;
